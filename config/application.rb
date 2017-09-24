fa08c3d295798b81aad0fd0ee39d0d41ed1cf5db
Camilo Flores <camilo.ignacio@gmail.com>
2017-09-24 01:15:31 -0300

    chore(CORS): include CORS config

--- config/application.rb
+++ config/application.rb
@@ -1,22 +1,24 @@
 require_relative 'boot'
 
 require 'rails/all'
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module CtaLy
   class Application < Rails::Application
     config.active_job.queue_adapter = :delayed_job
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          headers: :any,
+          expose: ['X-Page', 'X-PageTotal'],
+          methods: [:get, :post, :delete, :put, :options]
+      end
+    end
     config.i18n.default_locale = 'es-CL'
     config.i18n.fallbacks = [:es, :en]
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # config.assets.paths << Rails.root.join('vendor')
+    config.load_defaults 5.1
   end
 end
