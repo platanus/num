7cbe503b296e375b0b76160448d7a63a45ead211
Saratscheff <psaratscheff@gmail.com>
2018-02-17 14:31:48 -0300

    feat(username): index user by username + unique constrain

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,4 +37,5 @@ end
 #
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_username              (username) UNIQUE
 #
