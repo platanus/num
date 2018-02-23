8bb432b3c39da6e759f10549133109405d690a7c
Camilo Flores <camilo.ignacio@gmail.com>
2018-02-23 19:13:48 -0300

    fix(AccountUser): fixes blank username

--- app/values/form/account_user.rb
+++ app/values/form/account_user.rb
@@ -4,28 +4,23 @@ module Form
 
     attr_accessor :dni, :name, :email, :number, :account_type_id, :bank_id, :username
 
-
     def save
       password = SecureRandom.hex(8)
-      if username.nil? || username.empty?
-        username = nil
-      else
-        username.downcase!
-      end
+      @username = @username.present? ? @username.downcase : nil
 
       user = User.create!(
-        dni: dni,
-        name: name,
-        email: email,
+        dni: @dni,
+        name: @name,
+        email: @email,
         password: password,
         password_confirmation: password,
-        username: username
+        username: @username
       )
 
       user.accounts.create!(
-        number: number,
-        account_type_id: account_type_id,
-        bank_id: bank_id
+        number: @number,
+        account_type_id: @account_type_id,
+        bank_id: @bank_id
       )
     end
   end
